c723d206c73f7a1ce6d6dec8e96d68550729093b
XeniaNova <ksyu2009@yandex.ru>
2022-10-17 18:43:23 +0300

    Решение 27

---
+++ задача 27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Summ(int n);
+{
+int res = 0;
+
+    while (n > 0)
+    {
+        res = res + (n % 10);
+        n = n / 10;
+    }
+    return res;
+}
+    
+Console.Clear();
+Console.Write("Введите первое число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {N} равна {Summ(N)}. ");
